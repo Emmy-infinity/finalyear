f21bedb73c9fd72f2bd7f1421066473a18a2c2b0
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2024-03-02 14:05:16 +0300

    Update views.py

--- elearningplatform/views.py
+++ elearningplatform/views.py
@@ -15,7 +15,7 @@ def home(request):
 def quizz(request):
     question=Add_Questions.objects.all()
            
-    paginator=Paginator(question,50)
+    paginator=Paginator(question,180)
     page = request.GET.get('page', 1)
     try:
           users = paginator.page(page)
