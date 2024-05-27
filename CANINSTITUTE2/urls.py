275c78b0f16edfa0fda6d9a6e3f8e30b287fb366
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2024-05-27 17:40:02 +0300

    Update urls.py

--- CANINSTITUTE2/urls.py
+++ CANINSTITUTE2/urls.py
@@ -5,7 +5,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('elearningplatform.urls'),
-path('account/', include('django.contrib.auth.urls')),
+
 ]
 admin.site.site_header='CAN INSTITUTE ADMINISTRATION'
 admin.site.site_title='CONTENT MANAGEMENT'
