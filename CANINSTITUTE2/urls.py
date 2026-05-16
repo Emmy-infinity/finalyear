5d3430df9c96ff0c293da3f2aa5ed36470ac0b09
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-05-16 23:20:30 +0300

    fix: exempt favicon view from CSRF protection

--- CANINSTITUTE2/urls.py
+++ CANINSTITUTE2/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def favicon_view(request):
-    return HttpResponse(status=204)  # No content response, prevents 400 error
+    return HttpResponse(status=204)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
