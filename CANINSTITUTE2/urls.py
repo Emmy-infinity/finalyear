aca10f41340cb3e70ba82eed105af41b71bdae78
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-05-16 23:07:50 +0300

    fix: add favicon.ico route to prevent 400 errors

--- CANINSTITUTE2/urls.py
+++ CANINSTITUTE2/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('elearningplatform.urls')),
     path('account', include('django.contrib.auth.urls')),
+    path('favicon.ico', TemplateView.as_view(template_name='favicon.ico', content_type='image/x-icon')),
 ]
 
 # Serve static files and media files in production
