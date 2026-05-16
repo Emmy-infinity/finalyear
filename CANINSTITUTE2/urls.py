e23d5e5006c2faaf35e1d90a455d2a5fc1cde8cf
Emmy-infinity <113331587+Emmy-infinity@users.noreply.github.com>
2026-05-16 23:01:32 +0300

    fix: add static and media file serving for production

--- CANINSTITUTE2/urls.py
+++ CANINSTITUTE2/urls.py
@@ -1,14 +1,18 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('elearningplatform.urls')),
-path('account', include('django.contrib.auth.urls')),
-
+    path('', include('elearningplatform.urls')),
+    path('account', include('django.contrib.auth.urls')),
 ]
-admin.site.site_header='CAN INSTITUTE ADMINISTRATION'
-admin.site.site_title='CONTENT MANAGEMENT'
 
+# Serve static files and media files in production
+if settings.DEBUG is False:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+admin.site.site_header = 'CAN INSTITUTE ADMINISTRATION'
+admin.site.site_title = 'CONTENT MANAGEMENT'
